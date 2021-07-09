949e0e438440b0b6700779424514a8d1c30673b0
Lucas Sander <sable300@yahoo.de>
2021-07-09 15:13:20 +0200

    Projektdateien hinzufügen.

---
+++ RPI_GPIO/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Device.Gpio;
+
+namespace RPI_GPIO
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GpioController controller;
+
+
+        public MainWindow()
+        {
+
+            
+
+            try
+            {
+                controller = new GpioController();
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            Console.Write("Initilized.... \n");
+            InitializeComponent();
+        }
+
+        public void SetSelectedPin(int pin, Button btn)
+        {
+            try
+            {
+                if (!controller.IsPinOpen(pin))
+                {
+                    controller.OpenPin(pin, PinMode.Output);
+                }
+
+                string Status = controller.Read(pin).ToString();
+
+                MessageBox.Show(Status);
+
+                if (Status == "Low")
+                {
+                    controller.Write(pin, PinValue.High);
+                    btn.Background = Brushes.Green;
+                }
+                else
+                {
+                    controller.Write(pin, PinValue.Low);
+                    btn.Background = Brushes.Red;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Mouse_Click_Btn(object sender, MouseButtonEventArgs e)
+        {
+            Button BTN = (Button)sender;
+            SetSelectedPin(Convert.ToInt32(BTN.Content), BTN);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
